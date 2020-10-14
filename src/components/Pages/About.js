df8cc45e01dde039fd492d9666be003a61062e26
kobrabson <67922399+kobrabson@users.noreply.github.com>
2020-10-14 11:00:53 -0600

    mobile view fin and about me done

--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -7,51 +7,58 @@ function About() {
 			<div className="headers">About</div>
 			<div className="centering-about-container">
 				<div className="about-container">
-					<p>
-						<span>{''}</span>
-						Hello! My name is Kody Brabson! I’m originally from Austin, Texas but I moved to Utah to marry
-						my best friend, Alysse.
+					<p className="text-size-mid">
+						Hello! My name is Kody Brabson! I am thrilled at the prospect of working in this industry and
+						for your company. I am highly motivated and will bring a high level of enthusiasm and energy to
+						this position. But first, I would like to introduce myself to you and provide you with some
+						additional background information.
 					</p>
 					<br />
-					<p>
-						<span>{'   '}</span>
+					<p className="text-size-mid indent">
+						Though I am originally from Florida, I considered Texas my home as I moved to Austin when I was
+						in middle school. However, I now call Utah home as I moved there in 2018 to marry my best
+						friend, Alysse.
+					</p>
+					<br />
+					<p className="text-size-mid indent">
 						I began my professional life in culinary art five years ago when I attended and graduated from
-						Le Cordon Bleu. From there I moved over half the world to start cooking in France. That is where
-						I began my culinary adventure with some of the best chefs in northern France. Over this time I
-						honed my skills and my knives well enough to be an invaluable resource at any kitchen I went to.
-						Eventually training and mentoring others in their own path to becoming chefs when I return to
-						the state.
+						Le Cordon Bleu. I then continued my culinary adventure in northern France with some of the best
+						chefs in the world. I learned that in addition to technical kitchen skills, I needed to have
+						people management skills. Over time, I refined and sharpened both my soft skills and my knives
+						well enough to be an invaluable resource in any kitchen I worked. I eventually began training
+						and mentoring others in their own path to becoming chefs.
 					</p>
 					<br />
-					<p>
-						<span>{'   '}</span>
-						Since I was just a kid I have had an interest in technology and seeing it evolve over time. From
-						having the old flip phone and start of the internet to the smart phones we have now and the max
-						media outlets we have now. Only increased my interest in the tech world but never had the
-						opportunity or the resources to pursue a career in the tech world.
+					<p className="text-size-mid indent">
+						Since I was very young. I have had an interest in all forms of technology. I have loved being
+						able to watch and participate in such incredible advancements. My first phone was a flip phone
+						and it was cutting technology. Now, we literally walk around with a small computer in our hand –
+						a computer with 100,000 times the processing power of the computer that landed Apollo 11 on the
+						moon. Such implausible advances, which will no doubt continue, has only increased my interest
+						and fascination in the information technology sphere.
 					</p>
 					<br />
-					<p>
-						<span>{'   '}</span>
-						During the covid pandemic I lost my job because food and beverage was one of the first things to
-						close down. I couldn’t sit and wait for the pandemic to hopefully calm down enough to go back to
-						work. My wife and I talked about going back to school. The first thing that came to mind was web
-						development. After that I found Devmountain and almost immediately started. There I learn the
+					<p className="text-size-mid indent">
+						During the covid pandemic, my food and beverage related job was eliminated. I used that
+						opportunity to finally pursue a career in the technology information industry. After much
+						research, web development appeared to be a perfect match for my interests and skills. In
+						October, I completed the web development course at Devmountain certification. There I learn the
 						basic skills set of Javascript, React, HTML, CSS Node.js, SQl, postgreSQL, and express.js. With
-						the basic under my belt I have taken it upon myself to learn Angular, C#, and Java, just to name
-						a few.
+						the basic understanding of programming I have taken it upon myself to learn Angular, C#, and
+						Java, just to name a few.
 					</p>
 					<br />
-					<p>
-						<span>{'   '}</span>
-						Now as a full stack web developer I can build, design and craft sites. I am a self-focused,
-						hard-work, and goal-oriented indvisional. With your help I can hone the skills already know and
-						learn to use new skills to have a sharp set of web development knives at the ready.
+					<p className="text-size-mid indent">
+						As a full stack web developer I can build, design and craft websites. In addition to those
+						skills, I will bring passion for the industry and a commitment to continually learn and grow. My
+						newly acquired web development knives are ready to be sharpened and used.
 					</p>
 					<br />
-					<p>I look forward to working with you!</p>
+					<p className="text-size-mid">I look forward to talking with you soon</p>
 					<br />
-					<p>Andrew Kody Brabson</p>
+					<p className="text-size-mid">Sincerely,</p>
+					<br />
+					<p className="text-size-mid indent">Andrew Kody Brabson</p>
 					<br />
 				</div>
 			</div>
