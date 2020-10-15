145cc384078853f9a17a52763fe985f911fb9a3d
kobrabson <67922399+kobrabson@users.noreply.github.com>
2020-10-15 12:04:05 -0600

    Updates

---
+++ precache-manifest.408f9c41938120cafd086c132b527a09.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3220efd0832e7fcc86afa93a24c2d38",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "47215790e13576c426c2",
+    "url": "/portfolio/static/css/main.49818dbf.chunk.css"
+  },
+  {
+    "revision": "17583af1b4bc9ee1a2ad",
+    "url": "/portfolio/static/js/2.c416a347.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.c416a347.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47215790e13576c426c2",
+    "url": "/portfolio/static/js/main.2d11b84a.chunk.js"
+  },
+  {
+    "revision": "1b4c6a0a26d9afc8dd1a",
+    "url": "/portfolio/static/js/runtime-main.3a82f8da.js"
+  },
+  {
+    "revision": "638be90904f9f9248d3c69cb2992c562",
+    "url": "/portfolio/static/media/1280px-React-icon.svg.638be909.png"
+  },
+  {
+    "revision": "661612c73cba3e15cc1fa18f79fa89b8",
+    "url": "/portfolio/static/media/HTML5.661612c7.png"
+  },
+  {
+    "revision": "8c945728483f96acd6f768c134359181",
+    "url": "/portfolio/static/media/I95A2215 (2).8c945728.jpg"
+  },
+  {
+    "revision": "521444a28d1ac7003ece0c1f030dcd21",
+    "url": "/portfolio/static/media/Node.js_logo.521444a2.png"
+  },
+  {
+    "revision": "c7f9de597493163ed8ead34ee297bcc9",
+    "url": "/portfolio/static/media/RESTAPI.c7f9de59.png"
+  },
+  {
+    "revision": "584d9f322f08684db8ff2217731e474f",
+    "url": "/portfolio/static/media/bootstrap.584d9f32.png"
+  },
+  {
+    "revision": "a32f83aa2c675058e4a05a0fd4da05eb",
+    "url": "/portfolio/static/media/css-3-logo.a32f83aa.png"
+  },
+  {
+    "revision": "94ae2dcd4fa410811cab4e1fbb403340",
+    "url": "/portfolio/static/media/javascript-logo-.94ae2dcd.png"
+  },
+  {
+    "revision": "0fbd04cba8e061b55e9da307a880f8f3",
+    "url": "/portfolio/static/media/mongoDB.0fbd04cb.png"
+  },
+  {
+    "revision": "5886aa9761d099ff128c4ac99a78176f",
+    "url": "/portfolio/static/media/mySQL.5886aa97.png"
+  },
+  {
+    "revision": "d351ff6657da5d137a1d53d1e2d0703a",
+    "url": "/portfolio/static/media/phoneviewphoto.d351ff66.jpg"
+  },
+  {
+    "revision": "204806b11c28ba050a5c4dba44a2556a",
+    "url": "/portfolio/static/media/postgresql-logo.204806b1.png"
+  },
+  {
+    "revision": "1508814a5568181f7bbfb2c91c1c5826",
+    "url": "/portfolio/static/media/smallerProtfolio.1508814a.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.de28adb99f16f1fd7b7aa7078ca059a2.js"
+  "/portfolio/precache-manifest.408f9c41938120cafd086c132b527a09.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
